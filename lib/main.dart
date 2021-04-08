7f4e5244f9dca2959d82c6dbf1b6782047494beb
hafidardiansyah <hafidardiansyahh@gmail.com>
2021-04-09 06:33:53 +0700

    Update Onboarding Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'package:device_preview/device_preview.dart';
 
 import 'constants/theme.dart';
 import 'screens/onboarding_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+      DevicePreview(
+        // enabled: !kReleaseMode,
+        builder: (BuildContext context) => MyApp(), // Wrap your app
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -20,6 +26,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: themeData(),
+      locale: DevicePreview.locale(context), // Add the locale here
+      builder: DevicePreview.appBuilder, // Add the builder here
       home: OnboardingScreen(),
     );
   }
--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -10,14 +10,12 @@ class OnboardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: Text(
             'Absensi',
             style: displayMedium.copyWith(color: primary),
           ),
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          elevation: 0,
         ),
         body: Responsive(
           mobile: _mobile(context),
@@ -32,36 +30,48 @@ class OnboardingScreen extends StatelessWidget {
     const String _svgImage = 'assets/images/data_intro.svg';
     final Size _size = MediaQuery.of(context).size;
 
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SvgPicture.asset(
-            _svgImage,
-            width: _size.width,
-          ),
-          Text(
-            'Ayo kelola absensi kamu!',
-            style: displaySmall.copyWith(color: gray1),
-          ),
-          const SizedBox(height: 8.0),
-          Text(
-            'Lebih mudah dan efesien mengelola absensi kamu di sekolah.',
-            style: textSmall.copyWith(color: gray2),
-          ),
-          const SizedBox(height: 24.0),
-          ButtonWidget(
-            onPressed: () {},
-            text: "Lanjut",
-            minimumSize: 50,
-          ),
-          const SizedBox(height: 24.0),
-          Text(
-            'Dengan menggunakan aplikasi ini, kamu menyetujui Ketentuan Layanan dan Ketentuan Privasi.',
-            style: textXSmall.copyWith(color: gray2),
-          ),
-        ],
+    // print(_size.height);
+    // Placeholder(),
+    //  (16.0 / 812.0) * _size.height
+
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.only(
+          bottom: defaultPadding,
+          left: defaultPadding,
+          right: defaultPadding,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SvgPicture.asset(
+              _svgImage,
+              width: _size.width,
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+              'Ayo kelola absensi kamu!',
+              style: displaySmall.copyWith(color: gray1),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              'Lebih mudah dan efesien mengelola absensi kamu di sekolah.',
+              style: textSmall.copyWith(color: gray2),
+            ),
+            const SizedBox(height: 24.0),
+            ButtonWidget(
+              onPressed: () {},
+              text: "Lanjut",
+              minimumSize: 50,
+            ),
+            const SizedBox(height: 24.0),
+            Text(
+              'Dengan menggunakan aplikasi ini, kamu menyetujui Ketentuan Layanan dan Ketentuan Privasi.',
+              style: textXSmall.copyWith(color: gray2),
+            ),
+          ],
+        ),
       ),
     );
   }
